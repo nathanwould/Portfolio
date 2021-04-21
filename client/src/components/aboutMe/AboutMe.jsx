8d669e6db09f622b311b1e32cc493bc951df80ce
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2021-04-21 16:30:28 -0400

    added photo of me

--- client/src/components/aboutMe/AboutMe.jsx
+++ client/src/components/aboutMe/AboutMe.jsx
@@ -7,11 +7,15 @@ export default function AboutMe() {
   const { Title, Paragraph, Link } = Typography;
   return (
     <Typography id='about-me' class='about-me'>
+      <img
+        id='my-face'
+        alt='me'
+        src='https://i.imgur.com/DDCdVFPm.jpg' />
       <Title>Hi, I'm Nathan</Title>
       <Paragraph className='paragraph'>I'm a trombonist turned full-stack web developer whose goal is to take his unique experience gained from time working in the arts, food industry, and as a nonprofit administrator to solve real-world problems for users with code. </Paragraph>
-      <LineOutlined />
+      {/* <LineOutlined /> */}
       <Paragraph className='paragraph'>Whether building a full-stack application, performing on stage, or trying to make capers out of quince blossoms, I always bring my curiosity, creativity and perspective to every project to foster an open, collaborative environment. </Paragraph>
-      <LineOutlined />
+      {/* <LineOutlined /> */}
       <Paragraph className='paragraph'>If I'm not behind my computer coding, you can find me planting seed starts in my garden, reading fermentation or listening to <Link
           className='fonk'
           href='https://open.spotify.com/track/0ivhQEsgDPmAkLKlwWcR9l?si=1izCVg2_RcO7BmwLc-IDkg'

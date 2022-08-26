cc94733d10b29373695ef9c4b55b57b212ee306a
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2022-08-26 16:12:55 -0400

    fixed project card alignment

--- client/src/components/codewars/CodeWars.jsx
+++ client/src/components/codewars/CodeWars.jsx
@@ -3,7 +3,15 @@ import { Space, Typography, Card } from "antd"
 export default function CodeWars() {
   const { Title , Link } = Typography;
   return (
-    <Space id="codewars" direction="vertical" style={{ marginTop: "6rem" }}>
+    <Space
+      id="codewars"
+      direction="vertical"
+      style={{
+        marginTop: "6rem",
+        display: "flex",
+        alignItems: "center"
+      }}
+    >
       <div style={{marginBottom: '5rem'}}></div>
       <Title level={2}>Or get a feel for my problem-solving process:</Title>
       <Card
@@ -11,6 +19,7 @@ export default function CodeWars() {
         className="codewars-card"
         id="codewars"
         cover={<img alt='code wars' src='https://i.imgur.com/suX7xS6h.png' />}
+        style={{maxWidth: "80vh"}}
       >
         <p>See what kinds of problems I'm tackling when no one is looking over my sholder.</p>
         <Link href='https://www.codewars.com/users/nathanwould' target="_blank">Take a Look at my Profile</Link>

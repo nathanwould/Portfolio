c7228b694af9518c8b8f42506b52ed0fb895368c
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2023-01-13 15:40:49 -0500

    hopefully fixed it fr this time

--- client/src/App.js
+++ client/src/App.js
@@ -98,20 +98,26 @@ function App() {
         </Content>
         <Footer>
           <a
-            className='footer-link'
-            type='text'
-            href='https://drive.google.com/file/d/1U52trwO-KmX7DaAy_06knd4d4rLDna9e/view'
+            className="footer-link"
+            type="text"
+            rel="noreferrer"
+            target="_blank"
+            href='https://drive.google.com/file/d/1U52trwO-KmX7DaAy_06knd4d4rLDna9e/view?usp=sharing'
           ><SolutionOutlined /> CV
           </a>
           <a
-            className='footer-link'
-            type='text'
+            className="footer-link"
+            type="text"
+            rel="noreferrer"
+            target="_blank"
             href='https://github.com/nathanwould'
             ><GithubOutlined /> GitHub
           </a>
           <a
-            className='footer-link'
-            type='text'
+            className="footer-link"
+            type="text"
+            rel="noreferrer"
+            target="_blank"
             href='https://www.linkedin.com/in/nathan-wood-dev/'
           ><LinkedinOutlined /> LinkedIn
           </a>
